5571dd0f17be37581f32229ad44b15c53687bc85
MattFofo <matteo.segattini2@gmail.com>
2022-02-21 14:48:50 +0100

    prova struttura condizioni

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let userAge = prompt('Età');
 
 let KmRoute = prompt('Km da percorrere');
 
+let element = document.getElementById("ticket-price");
+
+
+
+if (userAge < 18) {
+    element.innerText =  (KmPrice * KmRoute) - ((KmPrice * KmRoute) * 0.2);
+
+}else if (userAge >= 65) {
+    element.innerText = (KmPrice * KmRoute) - ((KmPrice * KmRoute) * 0.4);
+
+}else if (18 >= userAge < 65) {
+    element.innerText =  (KmPrice * KmRoute);
+}    
