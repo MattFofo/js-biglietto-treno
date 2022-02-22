b9821a9de4cfbe1d5d85333407a4b9a56ca843d2
MattFofo <matteo.segattini2@gmail.com>
2022-02-22 13:03:52 +0100

    correzioni

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ let userAge = prompt('Età');
 
 let KmRoute = prompt('Km da percorrere');
 
-let element = document.getElementById("ticket-price");
+let elementOutput = document.getElementById("ticket-price");
+
+let DiscountMinors = 20 / 100;
+
+let DiscountSeniors = 40 / 100;
+
 
 
 
 if (userAge < 18) {
-    element.innerHTML =  ((KmPrice * KmRoute) - ((KmPrice * KmRoute) * 0.2)).toFixed(2);
+    elementOutput.innerHTML =  ((KmPrice * KmRoute) - ((KmPrice * KmRoute) * DiscountMinors)).toFixed(2);
 
 }else if (userAge >= 65) {
-    element.innerHTML = ((KmPrice * KmRoute) - ((KmPrice * KmRoute) * 0.4)).toFixed(2);
+    elementOutput.innerHTML = ((KmPrice * KmRoute) - ((KmPrice * KmRoute) * DiscountSeniors)).toFixed(2);
 
 }else if (18 >= userAge < 65) {
-    element.innerHTML =  (KmPrice * KmRoute).toFixed(2);
+    elementOutput.innerHTML =  (KmPrice * KmRoute).toFixed(2);
 }    
